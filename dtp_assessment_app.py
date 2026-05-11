a07661bf2387639b07e4317f24eec6edd6c02e05
24348-sudo <24348@burnside.school.nz>
2026-05-12 08:41:35 +1200

    neatened up user interface

--- dtp_assessment_app.py
+++ dtp_assessment_app.py
@@ -16,4 +16,16 @@ def print_all_cars():
     db.close()
 
 
-print_all_cars()
+while True:
+    user_input = input("""What would you like to do?
+    1. Print all racecar stats
+    2. Print car names
+    3. Print car names and their horsepower
+    4. Print car names and the races competed in
+    5. Exit
+
+    """)
+    if user_input == '1':
+        print_all_cars()
+    elif user_input == '5':
+        break
