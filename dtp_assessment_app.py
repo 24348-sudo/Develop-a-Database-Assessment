e2aeb6f9a7547404aa677815ad12513b2c2da3b5
24348-sudo <24348@burnside.school.nz>
2026-05-12 09:05:43 +1200

    now can print only cars, cars & hp + cars & race ratio

--- dtp_assessment_app.py
+++ dtp_assessment_app.py
@@ -10,22 +10,70 @@ def print_all_cars():
     sql = 'SELECT * FROM cars;'
     cursor.execute(sql)
     results = cursor.fetchall()
-    print("""Car""")
+    print("""   Cars                                 HP  Races  Wins""")
     for car in results:
-        print(car)
+        print(f"{car[1]:<38} {car[2]:<6} {car[3]:<4} {car[4]:<4}")
+    db.close()
+
+
+def print_car_names():
+    # printing car names
+    db = sqlite3.connect('best_racecars.db')
+    cursor = db.cursor()
+    sql = 'SELECT * FROM cars;'
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print("""    Cars""")
+    for car in results:
+        print(f"{car[1]}")
+    db.close()
+
+
+def print_cars_and_hp():
+    # printing car names and horsepower
+    db = sqlite3.connect('best_racecars.db')
+    cursor = db.cursor()
+    sql = 'SELECT * FROM cars;'
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print("""   Cars                                 HP""")
+    for car in results:
+        print(f"{car[1]:<38} {car[2]:<6}")
+    db.close()
+
+
+def print_cars_and_race_ratio():
+    # printing car name &  races competed in + won & maker
+    db = sqlite3.connect('best_racecars.db')
+    cursor = db.cursor()
+    sql = 'SELECT * FROM cars;'
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print("""   Cars                               Races  Wins""")
+    for car in results:
+        print(f"{car[1]:<38} {car[3]:<3}: {car[4]:<4}")
     db.close()
 
 
 while True:
-    user_input = input("""What would you like to do?
+    user_input = input("""
+What would you like to do?
     1. Print all racecar stats
     2. Print car names
-    3. Print car names and their horsepower
-    4. Print car names and the races competed in
+    3. Print car names and horsepower
+    4. Print car names and races and wins ratio
     5. Exit
 
     """)
     if user_input == '1':
         print_all_cars()
+    elif user_input == '2':
+        print_car_names()
+    elif user_input == '3':
+        print_cars_and_hp()
+    elif user_input == '4':
+        print_cars_and_race_ratio()
     elif user_input == '5':
         break
+    else:
+        print()
